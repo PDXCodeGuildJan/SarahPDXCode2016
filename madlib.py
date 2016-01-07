87a26ea19bddd3e525e8d78034ceb3f14169f58c
Sarah Fellows <sarahfellows6@gmail.com>
2016-01-07 14:58:40 -0800

    madlibs complete project

--- madlib.py
+++ madlib.py
@@ -1,29 +1,29 @@
 # Make a madlib that prompts the user for 5 works/phrases and then adds those to a silly story 
 
-madlib = "Today we went on a hunt for "
+madlib = "Today we went on a hunt for a "
 
 specific_object = input ("Please tell me an object: ")
 
-madlib = madlib + specific_object + "that can only be found in "
+madlib += specific_object + " that can only be found in "
 
 place = input ("Please tell me a your favorite place: ")
 
-madlib = madlib + place + "But to get to this place we had to "
+madlib += place + " But to get to this place we had to "
 
 action = input ("Please tell me an action: ")
 
-madlib = madlib + action + "really hard. Then we had to "
+madlib += action + " really hard. Then we had to "
 
 action2 = input ("Please tell me another action: ")
 
-madlib = madlib + action2 + "in order to make it there in time to catch the plane. Once we made it on the plane, the flight attendant asked if we would sing "
+madlib += action2 + " in order to make it there in time to catch the plane. Once we made it on the plane, the flight attendant asked if we would sing "
 
 song = input ("Please tell me your favorite song: ")
 
-madlib = madlib + song + "in order to get the plane moving.  After, we flew to our destination and jumped in a cab. The cab driver asked if we would "
+madlib += song + " in order to get the plane moving.  After, we flew to our destination and jumped in a cab. The cab driver asked if we would start "
 
 dance = input ("Please tell me your favorite type of dance: ")
 
-madlib = madlib + dance + "in order for him to drive us to our desination. Here, we found what we were looking for."
+madlib += dance + "ing in order for him to drive us to our desination. Here, we found what we were looking for."
 
 print(madlib)
