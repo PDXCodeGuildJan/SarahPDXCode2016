f02b43735ecbe61e48e972f76e08eb471654dbc7
Sarah Fellows <sarahfellows6@gmail.com>
2016-01-07 14:16:46 -0800

    begginning of madlib project

---
+++ madlib.py
@@ -0,0 +1,17 @@
+# Make a madlib that prompts the user for 5 works/phrases and then adds those to a silly story 
+
+madlib = "Today we went on a hunt for "
+
+specific_object = input ("Please tell me an object: ")
+
+madlib = madlib + specific_object + "that can only be found in "
+
+place = input ("Please tell me a your favorite place: ")
+
+madlib = madlib + place + "But to get to this place we had to "
+
+action = input ("Please tell me an action: ")
+
+madlib = madlib + action + "really hard. Then we had to "
+
+action2 = input ("Please tell me another action: ")
