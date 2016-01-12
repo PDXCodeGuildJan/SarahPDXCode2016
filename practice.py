f5b48917308d835095df81ee7511e1e842dc656d
Sarah Fellows <sarahfellows6@gmail.com>
2016-01-11 16:29:05 -0800

    practing loops, string, lists, etc.

--- practice.py
+++ practice.py
@@ -4,11 +4,18 @@ def main():
 	print("elephant"[3:6])
 
 	user_input = input("Please provide a word: ")
+	#this allows for the user to input a work 
 
 	middle_characters(user_input)
 	#function to be called in the future
 
 	loop(user_input)
+	#this will use whatever the user puts in for the loop
+
+	my_list=["cookies", "puppies", "rafting", "soft sheets", "pickles", "rock climbing"]
+	#this makes the list
+	loop(my_list)
+	#this passes the list to the function 
 
 
 def middle_characters(the_string):
@@ -23,11 +30,12 @@ def middle_characters(the_string):
 #Write a function that expects an input of a sequence, 
 #prints out the sequence, loops through the sequence, and prints out each element 
 	
-def loop(sequence):
-#defines the variable new_string 
-	print(sequence)
+def loop(my_list):
+#defines the variable loop
+
+	print(my_list)
 	#this prints the variable/sequence  
-	for x in sequence:
+	for x in my_list:
 		print(x)
 		#this loops through the sequence and prints each element 
 
