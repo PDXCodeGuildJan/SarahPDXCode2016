e1b29a4c14792c389449ee11a94ed5a9156518fb
Sarah Fellows <sarahfellows6@gmail.com>
2016-01-13 16:44:11 -0800

    First version of sort.py, no code just written out in english

---
+++ Algorithms/sort.py
@@ -0,0 +1,26 @@
+
+#define a function within this module called "selection_sort" that expects a list 
+# write a implementation of Selection Sort in this function 
+
+unsorted_list([6, 4, 9, 34, 89]) 
+	#Make a list 
+
+
+def selection_sort(unsorted_list):
+#define a function 
+
+	#Take the first item in the unsorted list 
+	smallest_Number=min(unsorted_list)
+
+	#Next, find the smallest item in the unsorted list and swap it with the first item in the unsorted list. 
+
+
+	#Shift the unsorted list to the next index.
+
+
+	#Repeat this process until list is complete. 
+
+
+selection_sort()
+
+
