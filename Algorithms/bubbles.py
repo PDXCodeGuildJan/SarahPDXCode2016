94bbfcc0b917b47e24576ba26914658d431a8707
Sarah Fellows <sarahfellows6@gmail.com>
2016-01-15 16:33:56 -0800

    Adding seperate file of bubbles made, adding sort.py which includes bubbles without all my notes from bubbles.sort

---
+++ Algorithms/bubbles.py
@@ -0,0 +1,90 @@
+unsorted_list=[6, 17, 9, 34, 4]
+
+print(unsorted_list)
+
+def bubble_sort(unsorted_list):
+#define a function 
+
+    listLength = len(unsorted_list)
+    # 1)Find the length of the list 
+
+    #2) Compare the value of the current index and compare it with the value of the next index. 
+    firstItem = 0 
+    
+    while firstItem < listLength -1:
+    #this starts the outter loop    
+        currentIndex = firstItem
+
+        while currentIndex < listLength - 1:
+        # need to establish an inner while loop - this says that while the current Index has any length
+        #to use for comparison, keep loop but decrease the length by one each time    
+
+            # 3) Determine if value of current index is is greater than value of next index. 
+            if unsorted_list[currentIndex] > unsorted_list[currentIndex+1]:
+
+                # 4) If it greater, swap with next index. If it is not greater, do nothing.
+                #unsorted_list[currentIndex], unsorted_list[currentIndex+1] = unsorted_list[currentIndex+1], unsorted_list[currentIndex]
+                tempSpot = unsorted_list[currentIndex]
+                unsorted_list[currentIndex] = unsorted_list[currentIndex+1]
+                unsorted_list[currentIndex+1] = tempSpot
+
+            currentIndex += 1
+            #this makes sure that the index does up by one index each loop
+
+        listLength -= 1     
+        #6) Once end is reached, move the end point in by one.  
+
+    return unsorted_list
+
+sorted_list = bubble_sort(unsorted_list)
+print(sorted_list)
+    
+"""
+
+Bubble Sort 
+Given a list: 
+
+1) Find the lenth of the list
+
+    2) Compare the value of the current index and compare it with the value of the next index. 
+    3) Determine if value of current index is is greater than value of next index. 
+    4) If it greater, swap with next index. If it is not greater, do nothing.
+
+    5) Change current index to next index, repeat process for the length of the list.
+
+6) Once end is reached, move the end point in by one. 
+
+7) Repeat process.
+
+while current_index < len(unsorted_list[first_number:]):
+#this moves 
+"""
+
+
+    #2) Compare the value of the current index and compare it with the value of the next index. 
+    
+    #for firstItem,value in enumerate(unsorted_list):
+    #this starts the outter loop    
+
+        #currentIndex = firstItem
+
+        #if currentIndex
+
+
+    #Does the currentIndex need to be identified as the lowest index?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Algorithms/sort.py
+++ Algorithms/sort.py
@@ -1,102 +1,129 @@
+def main():
+
+    unsorted_list = [6, 17, 9, 34, 4]
+
+    print("original", unsorted_list)
+    #Make a list 
+
+    sorted_list = selection_sort(unsorted_list)
+    print("selection", sorted_list)
+
+    sorted_list = bubble_sort(unsorted_list)
+    print("bubble", sorted_list)
 
 #define a function within this module called "selection_sort" that expects a list 
 # write a implementation of Selection Sort in this function 
-
-unsorted_list=[6, 17, 9, 34, 4]
-
-print(unsorted_list)
-	#Make a list 
-
-
 def selection_sort(unsorted_list):
 #define a function 
 
-	list_length = len(unsorted_list)
-	#this finds the length of the unsorted_list
+    list_length = len(unsorted_list)
+    #this finds the length of the unsorted_list
 
-	#Take the first index in the unsorted list 
-	first_number = 0
-	#this starts the index 
+    #Take the first index in the unsorted list 
+    first_number = 0
+    #this starts the index 
 
-	
+    #Repeat this process until list is complete - loop - move up to where you want the loop to start 
+    while first_number < list_length:
+    #while the index is less than the list length then:
+    #this is so that while the index still has numbers to compare to in the length then do the following:
+        current_index = first_number
+        lowest_index = first_number
 
-	#Repeat this process until list is complete - loop - move up to where you want the loop to start 
-	while first_number < list_length:
-	#while the index is less than the list length then:
-	#this is so that while the index still has numbers to compare to in the length then do the following:
-		current_index = first_number
-		lowest_index = first_number
+        while current_index < len(unsorted_list):
+        #while the index is less than the length of the the current index 
+            if unsorted_list[lowest_index] > unsorted_list[current_index]:
+            #if the value in lowest index of the unsorted list is greater than the value of the current index in the unsorted list:
+                lowest_index = current_index
 
-		while current_index < len(unsorted_list):
-		#while the index is less than the length of the the current index 
-			if unsorted_list[lowest_index] > unsorted_list[current_index]:
-			#if the value in lowest index of the unsorted list is greater than the value of the current index in the unsorted list:
-				lowest_index = current_index
-
-			current_index += 1 
-
-		#swap 
-		#current_index, first_number = first_number, current_index
-
-		
-		
-		temp = unsorted_list[lowest_index]
-		unsorted_list[lowest_index] = unsorted_list[first_number]
-		unsorted_list[first_number] = temp
-
-			#by calling the unsorted_list[value] it puts in a value instead of the index 
-
-		"""
-		Temp = A 
-		1st Index = B 
-		Lowest Num = c 
-
-		A=B
-		B=C 
-		C=A
-
-		"""
+            current_index += 1 
 
 
-			#lowest_index[current_index] = unsorted[lowest_index]
-		
+        #swap 
+        #current_index, first_number = first_number, current_index
 
-		#swap smallest number with the first item in the unsorted list. 
+        
+        temp = unsorted_list[lowest_index]
+        unsorted_list[lowest_index] = unsorted_list[first_number]
+        unsorted_list[first_number] = temp
 
-		#Update start of unsorted list - Shift the unsorted list to the next index.
-		first_number += 1
-		#this takes the index first_number and bumps up the index during the next loop around. 
-	return unsorted_list
+            #by calling the unsorted_list[value] it puts in a value instead of the index 
 
+        """
+        Temp = A 
+        1st Index = B 
+        Lowest Num = c 
 
-	
-sorted_list = selection_sort(unsorted_list)
-print(sorted_list)
+        A=B
+        B=C 
+        C=A
+
+        """
+        #swap smallest number with the first item in the unsorted list. 
+
+        #Update start of unsorted list - Shift the unsorted list to the next index.
+        first_number += 1
+        #this takes the index first_number and bumps up the index during the next loop around. 
+    return unsorted_list
 
 
 """
-
-Bubble Sort 
-
-1) Make a list 
-
-#Find the lenth of the list
-
-2) Compare the first two index's values and determine if the first index's value is greater than the second index's value. 
-If it isn't greater, then swap the two values. 
-
-3) Repeat the process for the length of the list.
-
-4) Once the list runs through the end of the length, move the end point in by one. 
-
-5) Repeat process.
-
-
-Find the first index's value is the first index greater than the value in the second index 
-
-
-while current_index < len(unsowhile current_index < len(unsorted_list[first_number:])
+---------------- Bubble Sort  ----------------------
 """
+
+def bubble_sort(unsorted_list):
+#defines the function 
+
+    # 1) Find the length of the list 
+    list_length = len(unsorted_list)
+
+    # 2) Compare the value of the current index and compare it with the value of the next index.
+    
+    first_item = 0 
+    #this says that the first index spot is now called first_item 
+
+    while first_item < list_length -1:
+    #this starts the outter loop - as long as the index is less than the length of the list 
+    #(subtract one to avoid infanite loop)
+
+        current_index = first_item
+        #this assigned current_index to be the first index - 0 
+
+
+        while current_index < list_length -1:
+        #starts the inner loop
+        # this says that while the current Index has any length
+        #to use for comparison, keep loop but decrease the length by one each time
+
+            # 3) Determine if value of current index is is greater than value of next index. 
+            if unsorted_list[current_index] > unsorted_list[current_index+1]:
+
+                tempSpot = unsorted_list[current_index]
+                unsorted_list[current_index] = unsorted_list[current_index+1]
+                unsorted_list[current_index+1] = tempSpot
+
+            current_index += 1
+
+        list_length -= 1
+
+    return unsorted_list
+
+
+main ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
