bfb1698e7400b5e7d721daf2643ce8dc16638f8a
Sarah Fellows <sarahfellows6@gmail.com>
2016-01-20 16:35:15 -0800

    merge functions completed with updated notes

--- Algorithms/merge.py
+++ Algorithms/merge.py
@@ -17,21 +17,30 @@ def merge_sort(unsorted):
 	# Given a list: 
 
 	# split the list into two halfs, if list length is more than 1 
+	#If the list length is more than 1... 
 	if len(unsorted) > 1:
+		#Find the middle of the unsorted listed
 		mid = len(unsorted) // 2 
-		left_list= unsorted[:mid]
-		right_list= unsorted[mid:]
+		#Determine what the left_list is by slicing the unsorted list by the 
+		#beginning to the middle [:mid]
+		left_list = unsorted[:mid]
+		#Determine what the right list is by slicing the unsorted list from the middle 
+		#to the end [mid:]
+		right_list = unsorted[mid:]
 
-		# sort the half using merge_sort 
+		# sort the half using merge_sort - start the recursion processing calling 
+		#merg_sort function again 
 		left_list = merge_sort(left_list)
 
-		# sort the second half using merg sort 
+		# sort the second half using merg sort  start the recursion processing calling 
+		#merg_sort function again 
 		right_list = merge_sort(right_list)
 
-		# merge the two sorted lists back together 
+		# merge the two sorted lists back together by calling the merge function defined below 
 		merged_list = merge(left_list,right_list)
 
-		#
+		#return what was found in the line above, calling the merge function left 
+		#below using the left and right list 
 		return merged_list
 
 	else:
@@ -66,6 +75,8 @@ def merge (left_list, right_list):
 			temp_list.append(right_list[right_i])
 			right_i += 1
 
+	#take the remaining of the each list using slicing to extend (adding the list to the list)
+	#it to the temp list 
 	temp_list.extend(left_list[left_i:])
 	temp_list.extend(right_list[right_i:])
 	
