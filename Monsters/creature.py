8c9bcf7fdab2367a6397a52bf9d120590b7064af
Sarah Fellows <sarahfellows6@gmail.com>
2016-02-05 10:38:08 -0800

    Creatures version to send Dre mid morning on Friday

--- Monsters/creature.py
+++ Monsters/creature.py
@@ -1,8 +1,13 @@
 """ Defines the Creature class, the base class of all living things in our game."""
 
 class Creature:
+	#When you forget the () after the class name, you are not creating a new one, you are calling a template - a 
+	#reference to the actual written code that is the function
 
-	#these are (const) variables, globals inside of the class 
+	#these are (const) variables, kind of like globals inside of the class, all CAPS means it is a constant, 
+	#don't modify these, they are the templates for the class
+	#properties and freatures defines on the class not on the object the class will create 
+	#these can be called by using TheNameOfTheClass.Name outside of the class -> ex: Creature.NORMAL
 	NORMAL = "normal"
 	ASLEEP = "asleep"
 	UNCONC = "unconscious"
@@ -21,8 +26,6 @@ class Creature:
 		self.weapon = None
 		self.special_abilities = {}
 		self.stats = {}
-		
-
 
 	def attack(self):
 
