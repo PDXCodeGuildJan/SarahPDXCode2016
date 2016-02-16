1c09f6ab18b713d938880672dc62fbbb6ab0f793
Sarah Fellows <sarahfellows6@gmail.com>
2016-02-16 14:00:16 -0800

    Sarah's version of MasterMind day 1

--- master_mind/master_controller.py
+++ master_mind/master_controller.py
@@ -2,22 +2,44 @@
 
 __author__ = "Sarah Fellows"
 
-import master_view
-#from master_view import show_rules, show_player_peg, show_key_peg, promt_user, win
-
 import master_model
+import master_view
+# alternative to import every class: from master_view import show_rules, show_player_peg, show_key_peg, promt_user, win
+#maybe use later on when we have all the classes identified 
+
 
 class MasterMind:
+    """ Defines the class MasterMind in the conroller function (logic)."""
 
         def play_game():
+            """ Has the user play the game of MasterMind. Loops through until 
+                the user wins or looses"""
             pass
 
         def check_win():
+            """ Defines the method check_win that will check if the player's 
+                choice matches the games randomized goal"""
             pass
 
         def eval_peg_color():
+            """Evaluates whether the player's peg colors matches any of the 
+                computers colors"""
             pass
 
         def eval_peg_position():
+            """Evaltues whether the player's matched colors also match any 
+                positions from the comp generated goal"""
             pass
+            
 
+        def generate_goal():
+
+            goal = []
+
+            for x in range(4):
+
+                player_peg = master_model.PlayerPeg(random.choice(master_model.MasterModel.peg_colors))
+
+                goal.append(player_peg)
+
+                return master_model.MasterModel.goal
--- master_mind/master_model.py
+++ master_mind/master_model.py
@@ -3,32 +3,54 @@
 __author__ = "Sarah Fellows"
 
 class MasterModel:
-""" This defines all the data that is changed and manipulated during the game """
+""" This defines all the data that is changed and manipulated during the game"""
     
     def __init__(self):
+    #this is where we put the global arguments for the game 
+
         self.guess_num = 1 #this is the stages of the game 
         self.goal = [] #computer generated random goal 
-        self.guesses = Guess() #appended list of all guesses made across whole game 
+        self.guesses = (Guess) #appended list of all guesses made across whole game 
+        self.peg_colors = ["Yellow", "Red", "Blue", "Green", "Black", "White"]
+        self.key_list = Guess.key
 
 
 class Guess:
-    """ stores history of guesss made accross the whole game - what has been presneted and what has been guessed 
-        - the player pegs and key pegs """
-    
+    """ stores history of guesss made accross the whole game - what has been 
+        presneted and what has been guessed  - the player pegs and key pegs """
+
+    #This needs to take in the guess from the player as well we the record of  
+    #computer generated suggests (key pegs)
+
     def __init__(self):
-        self.player_peg = [] #stores the string the player guesses 
+        self.player_peg = [] #stores the current player guess
         self.key_peg = [] #stores the computers response/suggestions for the next round
 
 class PlayerPeg:
-    """ Defines what the Player Pegs are"""
+    """ Defines the six color player Pegs"""
 
-    def __init__(self, color):
-        """ Stores the player peg properties """
-        self.color = color
+    # def __init__(self, color):
+    #     """ Stores the player peg properties. Initalized the pegs and colors """
+    #     self.color = color
+    #     self.index = 0 #Starts the value of the string at 0
+    #     self.locked = False 
+
+    # def __str__(self):
+    #     """Creates the information to display the colors and returns it in a 
+    #         string"""
+
+    #         temp = "("
+    #         temp += str(self.color[self.index])
+    #         temp += ")"
+
 
 class KeyPeg: 
-    """ Defines the Key Pegs returned from the evalation of player pegs choices """
+    """ Defines the Key Pegs returned from the evalation of player pegs 
+        choices """
+
+    #Takes in the player's guess and returns the key pegs based on choice
     
+    #Create the key pegs 
     SMALL_BLACK = "small_black"
     SMALL_WHITE = "small_white"
 
