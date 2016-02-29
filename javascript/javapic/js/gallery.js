a0d869f6d9fc5a290ed31d8cce56ff69f5acb83e
Sarah Fellows <sarahfellows6@gmail.com>
2016-02-29 13:51:44 -0800

    Working version of gallery minus clear

--- javascript/javapic/js/gallery.js
+++ javascript/javapic/js/gallery.js
@@ -4,18 +4,20 @@
 //Format: element.addEventListener('event', functionName[flowBool]); 
 window.addEventListener('load', showGalleryImgs); 
 
+
+
 // update the slogan to add a ", user_name", with the user's name
 
 
-//loop through the image folder and display each image in the folder
 
+
+//loop through the image folder and display each image in the folder
 function showGalleryImgs(){
     alert("This is a test popup"); 
 
-    // <img src="images/pdxcg_01.jpg">
+    // make empty lists to put the image strings in
     var oneImage = ""; 
-    var allImages = []; 
-
+    var allImages = ""; 
 
     //grab the id from HTML
     var showGalleryImgs = document.getElementById("gallery"); 
@@ -24,17 +26,16 @@ function showGalleryImgs(){
     // final = front + middle + end 
         // change the middle 
 
-    var imageStrBeg = "<img src='images/pdxcg_'"; 
-
-    var imageStrEnd = ".jpg/>"; 
-
-    
+    //By putting in <li> into the HTML string, it creates a <li> element before and after each img string. 
+    //This can be done with other HTML elements, esentially a whole webpage can be built in these strings (Pg 223)
+    var imageStrBeg = "<li><img src='images/pdxcg_"; 
+    //no need to indictae the middle string, we do so below
+    var imageStrEnd = ".jpg'/></li>"; 
 
     //iterate through each photo number 
-
     // loop through each image and put eat new one in a new string 
     for (var i = 1; i <= 60; i++){
-        // if the variable being iterated is less than 10 (or contained a 0)
+        // if the variable being iterated is less than 10 (i.e. containes a 0)
         if (i <= 9){
             // add 0 to the number 
             i = "0" + i;
@@ -42,30 +43,32 @@ function showGalleryImgs(){
 
         // elif if the number is 42, do something else 
 
+        console.log("Printing the image string to make sure it is correct", imageStrBeg, i, imageStrEnd) 
+
         // concatinate them back together in a string variable  
         oneImage = (imageStrBeg + i + imageStrEnd); 
             //put them into a array each time they iterate 
-        allImages.push(oneImage); 
 
+        // add one image to another and put them in the empty string made in the beginning 
+        allImages += oneImage; 
+     };
 
-    }; 
+    console.log(allImages); 
 
-    console.log(allImages)
-
-    // associate each new variable with a new photo 
+    // access each image stored in the element gallery 
+    showGalleryImgs.innerHTML = allImages
 
 }; 
-    
- 
-    // put them into an unordered list 
 
-    // make them display properly 
-    //create a variable that put the image in the spot and creates another variable 
+
 
 
 // add the functionality so that if a user clicks on an image, the lightbox appears with that image loaded in
   
+
+
 // when the lightbox is up, is the user clicks anywhere not on the image, the lightbox closes
+
 
 
 
