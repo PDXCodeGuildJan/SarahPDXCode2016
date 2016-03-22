f5ed8926ad75f2a4ee4fbf483bf358339ce5da9a
Sarah Fellows <sarahfellows6@gmail.com>
2016-03-22 14:38:50 -0700

    Complete basic URL, HTML, and basic set up for Portfolio project

--- django/port_assign/port_proj/port_home/views.py
+++ django/port_assign/port_proj/port_home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def port_home(request):
+    return render(request, 'port_home/index.html')
+
+
+# def adele(request):
+    # return render(request, 'port_home/index.html')
--- django/port_assign/port_proj/port_proj/wsgi.py
+++ django/port_assign/port_proj/port_proj/wsgi.py
@@ -1,3 +1,4 @@
+
 """
 WSGI config for port_proj project.
 
